7e87d866d801daafdd5c5e52f87790f33e7a4def
Scott Forward <slforward@creeradio.com>
2017-04-04 09:03:53 -0400

    added postgres url to routes

--- routes.py
+++ routes.py
@@ -4,7 +4,7 @@ from forms import SignupForm, LoginForm, AddressForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = # add your Heroku Postgres database URL here
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xzwleschuzpzms:b363afcc6a44ec9bdd1e16925701b99caecc56b23aee95ce0afa014ecffa2446@ec2-54-235-168-152.compute-1.amazonaws.com:5432/d47crsigacn00r'
 db.init_app(app)
 
 app.secret_key = "development-key"
